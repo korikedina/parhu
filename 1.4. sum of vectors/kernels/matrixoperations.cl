3e584657feda9d0dfbbd64b216fc56f232740447
korikedina <113836770+korikedina@users.noreply.github.com>
2025-02-25 11:31:24 +0100

    Add files via upload

--- 1.4. sum of vectors/kernels/matrixoperations.cl
+++ 1.4. sum of vectors/kernels/matrixoperations.cl
@@ -1,4 +1,4 @@
-__kernel void vecsum_kernel(__global float2* buffer1, __global float2* buffer2, __global float2* result, int n)
+__kernel void trans(__global float2* buffer1, __global float2* buffer2, __global float2* result, int n)
 {    
     float2 a = buffer1[get_global_id(0)]; 
     float2 b = buffer2[get_global_id(0)];
--- 1.4. sum of vectors/main.c
+++ 1.4. sum of vectors/main.c
@@ -98,17 +98,24 @@ int main(void)
     cl_kernel kernel = clCreateKernel(program, "vecsum_kernel", NULL);
 
     // Create the host buffer and initialize it
-    float * host_buffer = (float*)malloc(4 * sizeof(float)*2);
+    float *host_buffer1 = (float*)malloc(SAMPLE_SIZE * sizeof(float)*2);
+    float *host_buffer2 = (float*)malloc(SAMPLE_SIZE * sizeof(float)*2);
+    float *host_result = (float*)malloc(SAMPLE_SIZE * sizeof(float)*2);
     for (i = 0; i < 4; ++i) {
-        host_buffer[i] = i;
+        host_buffer1[i] = (float)i;
+        host_buffer2[i] = (float)(i+1);
     }
 
     // Create the device buffer
-    cl_mem device_buffer = clCreateBuffer(context, CL_MEM_READ_WRITE, SAMPLE_SIZE * sizeof(float)*2, NULL, NULL);
+    cl_mem device_buffer1 = clCreateBuffer(context, CL_MEM_READ_ONLY, SAMPLE_SIZE * sizeof(float)*2, NULL, NULL);
+    cl_mem device_buffer2 = clCreateBuffer(context, CL_MEM_READ_ONLY, SAMPLE_SIZE * sizeof(float)*2, NULL, NULL);
+    cl_mem device_result = clCreateBuffer(context, CL_MEM_WRITE_ONLY, SAMPLE_SIZE * sizeof(float)*2, NULL, NULL);
 
     // Set kernel arguments
-    clSetKernelArg(kernel, 0, sizeof(cl_mem), (void*)&device_buffer);
-    clSetKernelArg(kernel, 1, sizeof(int), (void*)&SAMPLE_SIZE);
+    clSetKernelArg(kernel, 0, sizeof(cl_mem), (void*)&device_buffer1);
+    clSetKernelArg(kernel, 1, sizeof(cl_mem), (void*)&device_buffer2);
+    clSetKernelArg(kernel, 2, sizeof(cl_mem), (void*)&device_result);
+    clSetKernelArg(kernel, 3, sizeof(int), (void*)&SAMPLE_SIZE);
 
     // Create the command queue
     cl_command_queue command_queue = clCreateCommandQueue(context, device_id, NULL, NULL);
@@ -116,20 +123,31 @@ int main(void)
     // Host buffer -> Device buffer
     clEnqueueWriteBuffer(
         command_queue,
-        device_buffer,
+        device_buffer1,
         CL_FALSE,
         0,
         SAMPLE_SIZE * sizeof(float)*2,
-        host_buffer,
+        host_buffer1,
         0,
         NULL,
         NULL
     );
 
+    clEnqueueWriteBuffer(
+        command_queue,
+        device_buffer2,
+        CL_FALSE,
+        0,
+        SAMPLE_SIZE * sizeof(float)*2,
+        host_buffer2,
+        0,
+        NULL,
+        NULL
+    );
+    
     // Size specification
-    size_t local_work_size = 256;
-    size_t n_work_groups = (SAMPLE_SIZE*2 + local_work_size + 1) / local_work_size;
-    size_t global_work_size = n_work_groups * local_work_size;
+    size_t local_work_size = 1;
+    size_t global_work_size = SAMPLE_SIZE;
 
     // Apply the kernel on the range
     err=clEnqueueNDRangeKernel(
@@ -152,11 +170,11 @@ int main(void)
     // Host buffer <- Device buffer
     err=clEnqueueReadBuffer(
         command_queue,
-        device_buffer,
+        device_result,
         CL_TRUE,
         0,
         SAMPLE_SIZE * sizeof(float)*2,
-        host_buffer,
+        host_result,
         0,
         NULL,
         NULL
@@ -165,10 +183,11 @@ int main(void)
         printf("Build error! Code: %d\n", err);
     
     }
-    clFinish(command_queue);
-    for (i = 0; i < 4; ++i) {
-        printf("[%d] = %f, ", i, host_buffer[i]);
+    for (i = 0; i < SAMPLE_SIZE*2; ++i) {
+        printf("[%d] = %f, ", i, host_result[i]);
     }
+    clFinish(command_queue);
+
 
     // Release the resources
     clReleaseKernel(kernel);
@@ -176,5 +195,6 @@ int main(void)
     clReleaseContext(context);
     clReleaseDevice(device_id);
 
-    free(host_buffer);
+    free(host_buffer1);
+    free(host_buffer2);
 }
