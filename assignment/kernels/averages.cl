b5827b056df72e727a0ff361cc96e8eb6231040c
korikedina <korikedina.edo@gmail.com>
2025-04-08 10:56:29 +0200

    works for 4 values (0408)

--- assignment/kernels/averages.cl
+++ assignment/kernels/averages.cl
@@ -1,21 +1,31 @@
-__kernel void avg(__global short4* buffer1, __global short4* buffer2, __global short4* result)
+__kernel void avg(__global short* buffer1, __global short* buffer2, __global short* result)
 {    
-    short4 left = buffer1[get_global_id(0)]; 
-    printf("l %u %u", left[0], left[0]);
-    short4 right = buffer2[get_global_id(0)];
-    printf("r %u %u", right[0], right[0]);
-    short4 subs;
-    short value;
-    for(int i=0; i<4; i++){
-        subs[i]=left[i]-right[i];
-        printf("h %d %d. %u %u\n", i, i, subs[i], subs[i]);
-    }
-    for(int i=0; i<4; i++){
-        value+=subs[i];
-    }
-    value/=4;
-    if (get_global_id(0) < 4) {
-        result[get_global_id(0)] = value;
+    // Simple fixed string printf first
+    
+    // Get work-item ID 
+    int gid = get_global_id(0);
+    
+    // Early exit for out-of-bounds
+    if (gid >= 4) {
+        return;
     }
 
+    // Get the values from the buffers
+    short4 left = buffer1[gid];  
+    short4 right = buffer2[gid];
+    short4 diff = {0, 0, 0, 0};
+
+    // Calculate the differences
+    for (int i = 0; i < 4; i++) {
+        diff[i] = left[i] - right[i];
+    }
+    
+    // Calculate the average
+    short avg = 0;
+    for (int i = 0; i < 4; i++) {
+        avg += diff[i];
+    }
+    avg = (short)(avg / 4);
+    // Store the result in the output buffer    
+    result[gid] = avg;
 }
--- assignment/main.c
+++ assignment/main.c
@@ -17,12 +17,13 @@ int main(void)
     WAV wav;
     read_wav_file("assets/remalomfold.wav", &wav);
 
-    /* for(int i = 0; i <= 6557501; i++){
+    for(int i = 0; i <= 6557501; i++){
         float left = (float)(wav.aud_data.samples[i].left) / 32767.5f;
         float right = (float)(wav.aud_data.samples[i].right) / 32767.5f;
+        float diff = left - right;
 
-        if(left-right!=0) printf("[%d]", left-right);
-    } */
+        //if((diff != 0.0f)!=0) printf("%d. [%f]", i, diff);
+    } 
 
     struct errorcode errorArray[90];
     readErrorsFromFile(errorArray);
@@ -32,31 +33,34 @@ int main(void)
 
     // Get platform
     cl_uint n_platforms;
-	cl_platform_id platform_id;
+    cl_platform_id platform_id;
     err = clGetPlatformIDs(1, &platform_id, &n_platforms);
-	if (err != CL_SUCCESS) {
+    if (err != CL_SUCCESS) {
         printErrorDetails(err, errorArray, 90);
-
-	}
+        return 0;
+    }
 
     // Get device
-	cl_device_id device_id;
-	cl_uint n_devices;
-	err = clGetDeviceIDs(
-		platform_id,
-		CL_DEVICE_TYPE_GPU,
-		1,
-		&device_id,
-		&n_devices
-	);
-	if (err != CL_SUCCESS) {
+    cl_device_id device_id;
+    cl_uint n_devices;
+    err = clGetDeviceIDs(
+        platform_id,
+        CL_DEVICE_TYPE_GPU,
+        1,
+        &device_id,
+        &n_devices
+    );
+    if (err != CL_SUCCESS) {
         printErrorDetails(err, errorArray, 90);
-
-		return 0;
-	}
+        return 0;
+    }
 
     // Create OpenCL context
-    cl_context context = clCreateContext(NULL, n_devices, &device_id, NULL, NULL, NULL);
+    cl_context context = clCreateContext(NULL, n_devices, &device_id, NULL, NULL, &err);
+    if (err != CL_SUCCESS) {
+        printErrorDetails(err, errorArray, 90);
+        return 0;
+    }
 
     // Build the program
     const char* kernel_code = load_kernel_source("kernels/averages.cl", &error_code);
@@ -64,7 +68,12 @@ int main(void)
         printf("Source code loading error!\n");
         return 0;
     }
-    cl_program program = clCreateProgramWithSource(context, 1, &kernel_code, NULL, NULL);
+    cl_program program = clCreateProgramWithSource(context, 1, &kernel_code, NULL, &err);
+    if (err != CL_SUCCESS) {
+        printErrorDetails(err, errorArray, 90);
+        return 0;
+    }
+
     const char options[] = "-D SET_ME=1234";
     err = clBuildProgram(
         program,
@@ -94,157 +103,196 @@ int main(void)
             build_log,
             &real_size
         );
-        // build_log[real_size] = 0;
-        printf("Real size : %d\n", real_size);
+        printf("Real size : %zu\n", real_size);
         printf("Build log : %s\n", build_log);
         free(build_log);
+        clReleaseProgram(program);
+        clReleaseContext(context);
+        clReleaseDevice(device_id);
         return 0;
     }
-    size_t sizes_param[10];
-    size_t real_size;
-    err = clGetProgramInfo(
-        program,
-        CL_PROGRAM_BINARY_SIZES,
-        10,
-        sizes_param,
-        &real_size
-    );
-    //printf("Real size   : %d\n", real_size);
-    //printf("Binary size : %d\n", sizes_param[0]);
-    cl_kernel kernel = clCreateKernel(program, "avg", NULL);
 
-    // Create the host buffer and initialize it
-    int16_t *host_buffer1 = (int16_t*)malloc( sizeof(int16_t)*4);
-    int16_t *host_buffer2 = (int16_t*)malloc( sizeof(int16_t)*4);
+    // Create the kernel
+    cl_kernel kernel = clCreateKernel(program, "avg", &err);
+    if (err != CL_SUCCESS) {
+        printErrorDetails(err, errorArray, 90);
+        clReleaseProgram(program);
+        clReleaseContext(context);
+        clReleaseDevice(device_id);
+        return 0;
+    }
+
+    // Create the host buffer and initialize it - change to short4 size
+    int16_t *host_buffer1 = (int16_t*)malloc(sizeof(int16_t) * 4); // One short4
+    int16_t *host_buffer2 = (int16_t*)malloc(sizeof(int16_t) * 4);
     if (host_buffer1 == NULL || host_buffer2 == NULL) {
         printf("Memory allocation failed!\n");
-        exit(1);  // or handle the error
+        clReleaseKernel(kernel);
+        clReleaseProgram(program);
+        clReleaseContext(context);
+        clReleaseDevice(device_id);
+        exit(1);
     }
-    int16_t *host_result = (int16_t*)malloc(sizeof(int16_t));
-    int s=22775;
+    int16_t *host_result = (int16_t*)malloc(sizeof(int16_t) * 4); // Space for one short4
 
-     /* for(i=0; i<25500; i++){
-        if(wav.aud_data.samples[i].left!=0){
-            printf("%d\n", i);
-            printf("L %u\n", wav.aud_data.samples[i].left);
-            printf("R %u\n", wav.aud_data.samples[i].right);
-        }
-    }  */
+    int s = 3893317;
 
-    //while(s<6557501){
-        printf("%d. result\n", s);
+    // Print some surrounding samples for context
+    for (i = s-2; i < s+6; i++) {
+        printf("Sample[%d]: L=%d R=%d\n", 
+               i, 
+               wav.aud_data.samples[i].left,
+               wav.aud_data.samples[i].right);
+    }
 
-        for (i = s; i < s+4; i++) {
-            printf("%d.\n", i-s);
-            int16_t left=wav.aud_data.samples[i].left;
-            //printf("L %u\n", left);
-            int16_t right=wav.aud_data.samples[i].right;
-            //printf("R %u\n", right);
-            host_buffer1[i-s] = i-s;
-            printf("bl %u\n",host_buffer1[i-s]);
-            host_buffer2[i-s] = i-s;
-            printf("br %u\n",host_buffer2[i-s]);
-
-        }
-
-        s+=4;
-
-        // Create the device buffer
-        cl_mem device_buffer1 = clCreateBuffer(context, CL_MEM_READ_ONLY, SAMPLE_SIZE * sizeof(int16_t)*4, NULL, &err);
-        cl_mem device_buffer2 = clCreateBuffer(context, CL_MEM_READ_ONLY, SAMPLE_SIZE * sizeof(int16_t)*4, NULL, &err);
-        cl_mem device_result = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(int16_t), NULL, &err);
-        if(err!=CL_SUCCESS){
-            printErrorDetails(err, errorArray, 90);
-        }
-
-        // Set kernel arguments
-        clSetKernelArg(kernel, 0, sizeof(cl_mem), (void*)&device_buffer1);
-        clSetKernelArg(kernel, 1, sizeof(cl_mem), (void*)&device_buffer2);
-        clSetKernelArg(kernel, 2, sizeof(cl_mem), (void*)&device_result);
-
-        // Create the command queue
-        cl_command_queue command_queue = clCreateCommandQueue(context, device_id, NULL, NULL);
-
-        // Host buffer -> Device buffer
-        clEnqueueWriteBuffer(
-            command_queue,
-            device_buffer1,
-            CL_FALSE,
-            0,
-            SAMPLE_SIZE * sizeof(int16_t)*4,
-            host_buffer1,
-            0,
-            NULL,
-            NULL
-        );
-
-        clEnqueueWriteBuffer(
-            command_queue,
-            device_buffer2,
-            CL_FALSE,
-            0,
-            SAMPLE_SIZE * sizeof(int16_t)*4,
-            host_buffer2,
-            0,
-            NULL,
-            NULL
-        );
+    // Populate buffers with actual audio data as short4 vectors
+    for (i = 0; i < 4; i++) {
+        // Print input data for debugging
+        printf("Input left[%d]: %d\n", i, wav.aud_data.samples[s+i].left);
+        printf("Input right[%d]: %d\n", i, wav.aud_data.samples[s+i].right);
         
-        // Size specification
-        size_t local_work_size = 1;
-        size_t global_work_size = 1;
+        // Each vector has 4 components
+        host_buffer1[i] = wav.aud_data.samples[s+i].left;
+        host_buffer2[i] = wav.aud_data.samples[s+i].right;
+    }
 
-        // Apply the kernel on the range
-        err=clEnqueueNDRangeKernel(
-            command_queue,
-            kernel,
-            1,
-            NULL,
-            &global_work_size,
-            &local_work_size,
-            0,
-            NULL,
-            NULL
-        );
-        if (err != CL_SUCCESS) {
-            printErrorDetails(err, errorArray, 90);
-        }
+    // Create the device buffer - adjust size for short4
+    cl_mem device_buffer1 = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(int16_t) * 4, NULL, &err);
+    cl_mem device_buffer2 = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(int16_t) * 4, NULL, &err);
+    cl_mem device_result = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(int16_t) * 4, NULL, &err);
+    if(err != CL_SUCCESS){
+        printErrorDetails(err, errorArray, 90);
+        goto cleanup;
+    }
 
-        // Host buffer <- Device buffer
-        err=clEnqueueReadBuffer(
-            command_queue,
-            device_result,
-            CL_TRUE,
-            0,
-            SAMPLE_SIZE * sizeof(int16_t),
-            host_result,
-            0,
-            NULL,
-            NULL
-        );
-        if (err != CL_SUCCESS) {
-            printErrorDetails(err, errorArray, 90);
-        }
+    // Set kernel arguments with error checking
+    err = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void*)&device_buffer1);
+    if (err != CL_SUCCESS) {
+        printErrorDetails(err, errorArray, 90);
+        goto cleanup;
+    }
+    err = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void*)&device_buffer2);
+    if (err != CL_SUCCESS) {
+        printErrorDetails(err, errorArray, 90);
+        goto cleanup;
+    }
+    err = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void*)&device_result);
+    if (err != CL_SUCCESS) {
+        printErrorDetails(err, errorArray, 90);
+        goto cleanup;
+    }
 
-        printf("R %u\n", host_result);
+    // Create the command queue with appropriate properties
+    cl_command_queue command_queue = clCreateCommandQueue(context, device_id, 0, &err);
+    if (err != CL_SUCCESS) {
+        printErrorDetails(err, errorArray, 90);
+        goto cleanup;
+    }
 
-        clFinish(command_queue);
+    // Add work size specifications
+    size_t global_work_size = 1;  // Start with one work-item
+    size_t local_work_size = 1;   
 
-    //}
+    // Add event synchronization
+    cl_event write_event1, write_event2;
+    
+    // Write buffers with events
+    err = clEnqueueWriteBuffer(
+        command_queue,
+        device_buffer1,
+        CL_FALSE,
+        0,
+        sizeof(int16_t) * 4,
+        host_buffer1,
+        0,
+        NULL,
+        &write_event1
+    );
+    if (err != CL_SUCCESS) {
+        printErrorDetails(err, errorArray, 90);
+        goto cleanup;
+    }
 
-    // Release the resources
-    // Add after clFinish()
-    clReleaseMemObject(device_buffer1);
-    clReleaseMemObject(device_buffer2);
-    clReleaseMemObject(device_result);
-    clReleaseKernel(kernel);
-    clReleaseCommandQueue(command_queue); 
-    clReleaseProgram(program);
-    clReleaseContext(context);
-    clReleaseDevice(device_id);
+    err = clEnqueueWriteBuffer(
+        command_queue,
+        device_buffer2,
+        CL_FALSE,
+        0,
+        sizeof(int16_t) * 4,
+        host_buffer2,
+        0,
+        NULL,
+        &write_event2
+    );
+    if (err != CL_SUCCESS) {
+        printErrorDetails(err, errorArray, 90);
+        goto cleanup;
+    }
 
+    // Create event list for kernel dependencies
+    cl_event write_events[2] = {write_event1, write_event2};
 
-    free(host_buffer1);
-    free(host_buffer2);
-    free(host_result);
+    // Ensure writes complete before kernel execution
+    err = clWaitForEvents(2, write_events);
+    if (err != CL_SUCCESS) {
+        printErrorDetails(err, errorArray, 90);
+        goto cleanup;
+    }
+
+    // Launch kernel after writes complete
+    cl_event kernel_event;
+    err = clEnqueueNDRangeKernel(
+        command_queue,
+        kernel,
+        1,
+        NULL,
+        &global_work_size,
+        &local_work_size,
+        2,           // Wait for both writes
+        write_events,
+        &kernel_event
+    );
+    if (err != CL_SUCCESS) {
+        printErrorDetails(err, errorArray, 90);
+        goto cleanup;
+    }
+
+    // Host buffer <- Device buffer
+    err = clEnqueueReadBuffer(
+        command_queue,
+        device_result,
+        CL_TRUE,
+        0,
+        sizeof(int16_t) * 4,  // Size for short4 result
+        host_result,
+        0,
+        NULL,
+        NULL
+    );
+    if (err != CL_SUCCESS) {
+        printErrorDetails(err, errorArray, 90);
+        goto cleanup;
+    }
+
+    // Print all components of the result
+    printf("Result: %d\n", 
+           *host_result);
+
+    clFinish(command_queue);
+
+cleanup:
+    // Release all resources
+    if (host_buffer1) free(host_buffer1);
+    if (host_buffer2) free(host_buffer2);
+    if (host_result) free(host_result);
+    if (device_buffer1) clReleaseMemObject(device_buffer1);
+    if (device_buffer2) clReleaseMemObject(device_buffer2);
+    if (device_result) clReleaseMemObject(device_result);
+    if (kernel) clReleaseKernel(kernel);
+    if (command_queue) clReleaseCommandQueue(command_queue);
+    if (program) clReleaseProgram(program);
+    if (context) clReleaseContext(context);
+    if (device_id) clReleaseDevice(device_id);
+
+    return err == CL_SUCCESS ? 0 : 1;
 }
